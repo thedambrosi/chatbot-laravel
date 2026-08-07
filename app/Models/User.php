f73c71634da8aa96bd3be188c002c8bd1ad5b438
Gabriel Dambrosi <gabrieldambrosidesweb@gmail.com>
2026-08-07 01:01:28 -0300

    feat: adiciona model Message e relacionamento com User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -16,6 +17,11 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
 
     /**
      * Get the attributes that should be cast.
